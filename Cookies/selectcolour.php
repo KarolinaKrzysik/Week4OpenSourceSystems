2a6ad6188562ad58fe38df747351ec75895922c1
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-03-02 00:35:36 +0000

    Week 4 task 1 Cookies

---
+++ Cookies/selectcolour.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Select colour Page</title>
+    </head>
+    <body>
+        <?php 
+            $cookie_name = "select_quantity";
+            setcookie($cookie_name, $_POST["selqty"]);
+        ?>
+        <form action = "confirmation.php" method = "POST">
+            Select the colour for the <?php echo $_POST["selqty"]?> widgets you are ordering
+            <select name = "selcolour">
+                <option>white</option>
+                <option>red</option>
+                <option>yellow</option>
+                <option>green</option>
+                <option>blue</option>
+            </select>
+            <br/><br/>
+            <input type = "submit" value = "Buy"/>
+        </form>
+    </body>
+</html>
